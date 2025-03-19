8fb22b76dab0075de89ae5abeebb3e06975b7807
shahrazadrapalli <shahrazadrapalli@gmail.com>
2025-03-19 21:26:45 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,27 @@
+<?php
+// Addition
+function addition($a, $b) {
+return $a + $b;
+}
+
+// Subtraction
+function subtraction($a, $b) {
+return $a - $b;
+}
+
+// Multiplication
+function multiplication($a, $b) {
+return $a * $b;
+}
+
+// Division
+function division($a, $b) {
+return $a / $b;
+}
+
+// Modulus
+function modulus($a, $b) {
+return $a % $b;
+}
+
+?>
