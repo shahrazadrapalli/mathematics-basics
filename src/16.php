2b07d02799f5a64db2d061d712d4d6f1ac3596f1
shahrazadrapalli <shahrazadrapalli@gmail.com>
2025-03-23 10:52:04 +0000

    Add 16.php

---
+++ src/16.php
@@ -0,0 +1,8 @@
+<?php
+// This is a simple PHP script to demonstrate basic arithmetic operations.
+// Example: $result = 5 + 3;
+// The result will be 8
+
+$result = 5 + 3;
+
+echo "The sum of 5 and 3 is: " . $result;
