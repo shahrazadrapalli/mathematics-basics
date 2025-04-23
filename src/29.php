8c726b35968f2a530a9d4807a652973fc33c4852
shahrazadrapalli <shahrazadrapalli@gmail.com>
2025-04-23 11:27:15 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize variables
+$a = 5;
+$b = 10;
+
+// Perform some math operations
+$c = $a + $b * 2;
+$d = $c - $a / $b;
+
+// Output the results
+echo "Result of a + b * 2: $d\n";
+?>
