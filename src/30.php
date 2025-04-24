007f83655b558da5252d41c1bc4cac2e71c336ea
shahrazadrapalli <shahrazadrapalli@gmail.com>
2025-04-24 15:51:33 +0000

    Add 30.php

---
+++ src/30.php
@@ -0,0 +1,14 @@
+<?php
+// Define variables
+$sentence = "This is a sentence.";
+$word1 = "is";
+$word2 = "a";
+$word3 = "sentence.";
+
+// Compare words
+if ($word1 === $word2 && $word2 === $word3) {
+    echo "All three words are the same.";
+} else {
+    echo "The words are not all the same.";
+}
+?>
