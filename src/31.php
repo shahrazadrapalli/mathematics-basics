884e33dc44ca0e1bb735b00964edbaea0a695fc9
shahrazadrapalli <shahrazadrapalli@gmail.com>
2025-04-25 16:41:10 +0000

    Add 31.php

---
+++ src/31.php
@@ -0,0 +1,8 @@
+<?php
+// This PHP script is designed to demonstrate basic operations and variables.
+$number1 = 5; // First number
+$operator = '+'; // Operator (+ or -)
+$number2 = 3; // Second number
+
+echo "The result of $number1 $operator $number2 is: $number1 + $number2";
+?>
