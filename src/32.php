6c4cd0d3dd5692cf37b36215b9057eeb717ae8b3
shahrazadrapalli <shahrazadrapalli@gmail.com>
2025-04-26 01:46:28 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,19 @@
+<?php
+// Example PHP script to demonstrate basic arithmetic operations and conditional statements
+
+# Addition
+echo "The sum is: " . 10 + 5;
+
+# Subtraction
+echo "The difference is: " . (10 - 5);
+
+# Multiplication
+echo "The product of 2 and 3 is: " . 2 * 3;
+
+// Example conditional statement
+$age = 18;
+if ($age >= 18) {
+    echo "You are old enough to vote.";
+} else {
+    echo "Sorry, you need to be at least 18 years old to vote.";
+}
